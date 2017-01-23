f8899e5a5eae02f4c4ed6823390c03d421ebd69a
Yi Liu <yxl605@cs.bham.ac.uk>
2017-01-23 11:27:13 +0000

    Audio

---
+++ audio/src/MusicTest.java
@@ -0,0 +1,37 @@
+import java.awt.Toolkit;
+
+public class MusicTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//java.awt.Toolkit.getDefaultToolkit().beep(); 
+		//java.awt.Toolkit.getDefaultToolkit().beep(); 
+		//java.awt.Toolkit.getDefaultToolkit().beep();        
+
+		/*for(int i = 1;i <= 100; i++ )
+		{
+		Toolkit.getDefaultToolkit().beep();
+		}*/
+		//Music test = new Music();
+		//test.play();
+		//Music.play();
+		
+		//Music.money();
+		//Music m = new Music();
+		//m.cash_register();
+		
+		
+		//m.score_board_music();
+		
+		//Music.score_board();
+		Music.cash_register();
+		//Music.potwak();
+		//Music.score_board_music();
+		//Music.score_board();
+		
+		//Music.bgm();
+		//Music m = new Music();
+		//m.BGM();
+	}
+
+}
